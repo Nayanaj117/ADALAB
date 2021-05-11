d62c8a75bb633a63c1e3eae1761b9bcabbe9bedc
Nayana J <68220099+Nayanaj117@users.noreply.github.com>
2021-05-11 09:56:27 +0530

    Update t_search.c

--- WEEK3/t_search.c
+++ WEEK3/t_search.c
@@ -1,105 +1,54 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<time.h>
 
-void generateArray(int arr[10], int n)
-{
-	for(int i = 0; i<n; i++)
-		arr[i] = (rand()%10000);
-}
-void print(int *arr, int n)
-{
-	printf("\nThe elements in the array are:\n");
-	for(int i = 0; i<n; i++)
-		printf("%d\t", arr[i]);
-}
 
-int LinearSearch(int* arr, int n, int i, int e)
-{
-	if(i==n)
-		return -1;
-	if(arr[i] == e)
-		return i;
-	return LinearSearch( arr, n, i+1, e);
+unsigned int recursiveLS(unsigned int arr[],unsigned int n,unsigned int key){
+    if (arr[n]== key){
+        printf("found at index %u\n\n",n);
+    }
+    else{
+        return recursiveLS(arr,n-1,key);
+    }
 }
-
-void Sort(int* arr, int n)
-{
-	for(int i = 0; i<n; i++)
-	{
-		for(int j = i+1; j<n; j++)
-		{
-			if(arr[i]>arr[j])
-			{
-				int t = arr[i];
-				arr[i] = arr[j];
-				arr[j] = t;
-			}
-		}
-	}
-}
-int BinarySearch(int* arr, int low,int mid, int high, int e)
-{
-	if(low == high)
-		return -1;
-	if(e==arr[mid])
-	{
-		return mid;
-	}
-	else if(e>arr[mid])
-	{
-		return BinarySearch(arr, mid, (int)(high+low)/2, high, e);
-	}
-	else
-	{
-		return BinarySearch(arr, low, (int)(high-low)/2, mid, e);
-	}
-}
+unsigned int recursiveBS(unsigned int arr[],unsigned int low,unsigned int high,unsigned int key){
+    unsigned int mid = (low+high)/2;
+    if(low<=high){
+        if(arr[mid]==key){
+        printf("Found at index %u\n\n",mid);
+        }
+        else if(arr[mid]<key){
+            recursiveBS(arr,mid+1,high,key);
+        }
+        else{
+           recursiveBS(arr,low,mid-1,key);
+        }
 
 
-int main()
-{
-	int *arr, ch=10, n;
-	clock_t start_l, start_b, end_l, end_b;
-	srand(time(0));
-	while(ch!=3)
-	{
-		printf("\n\n\n\t1]Linear Search\n\t2]Binary Search\n\t3]Exit\nEnter Choice:\t");
-		scanf("%d", &ch);
-		if(ch==3)
-			break;
-		printf("\nEnter the size of Array:\t");
-		scanf("%d", &n);
-		generateArray(arr, n);
-		print(arr, n);
-		int e, res;
-		switch(ch)
-		{
-			case 1: printf("\nEnter the search element:	");
-					scanf("%d", &e);
-					start_l = clock();
-					for (int c = 1; c <= 32767; c++) 
-						for (int d = 1; d <= 32767; d++) { }
-					res = LinearSearch(arr, n, 0, e);
-					end_l = clock();
-					break;
-			case 2: printf("\nEnter the search element:	");
-					scanf("%d", &e);
-					start_b = clock();
-					for (int c = 1; c <= 32767; c++)
-						for (int d = 1; d <= 32767; d++) { }
-					Sort(arr, n);
-					printf("\n\nSorted array is");
-					print(arr, n);
-					res = BinarySearch(arr, 0, (int)(n/2), n, e);
-					end_b = clock();
-					break;
-			default: printf("\nEnter Valid Choice\n\n\n");
-		}
-		if(res <0)
-			printf("\nElement not found!");
-		else printf("\nThe Element found at index %d", res);
-	}
-	printf("\nTime taken for binary search: %Lf", (long double)(end_b - start_b)/CLOCKS_PER_SEC);
-	printf("\nTime taken for linear search: %Lf", (long double)(end_l - start_l)/CLOCKS_PER_SEC);
-	}
+    }
+     else {
+        return -1;
+     }
+}
+
+void main(){
+    unsigned int n,key,low=0;
+    clock_t start,end;
+    printf("Enter the no of elements\n");
+    scanf("%u",&n);
+    printf("Enter the element to be searched\n");
+    scanf("%u",&key);
+    unsigned int arr[n];
+    for(unsigned int i=0;i<n;i++){
+        arr[i]=i;
+    }
+    start = clock();
+    recursiveLS(arr,n,key);
+    end = clock();
+    double time_taken = ((double)end-start)/CLOCKS_PER_SEC;
+    printf("Time taken to search %u using linear search technique is %f sec\n",key,time_taken);
+    start = clock();
+    recursiveBS(arr,low,n,key);
+    end = clock();
+    time_taken = ((double)end-start)/CLOCKS_PER_SEC;
+    printf("Time taken to search %u using binary search technique is %f sec\n",key,time_taken);
+}
